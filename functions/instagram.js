6a8bfa039c6aa02f1eee437be14092280b2f6c09
Keisuke Shinokita <keisuke@shinokita-mac1.ims.ac.jp>
2025-01-16 19:02:45 +0900

    Initial commit

---
+++ functions/instagram.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  try {
+    const response = await fetch(
+      `https://graph.facebook.com/v21.0/17841469568309282?fields=name,media.limit(12){caption,media_url,thumbnail_url,permalink,like_count,comments_count,media_type}&access_token=${process.env.INSTAGRAM_TOKEN}`
+    );
+    
+    const data = await response.json();
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' })
+    };
+  }
+};
